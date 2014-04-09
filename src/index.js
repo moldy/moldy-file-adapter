6833db06fd3c535b61dae374185eee7fa8dce22d
David Tsuji <david@flodge.com>
2014-04-10 08:35:24 +1000

    removed commented code

--- src/index.js
+++ src/index.js
@@ -74,40 +74,4 @@ module.exports = function (_model, _data, _method, _url, _callback) {
 
 	});
 
-	// if (/get/i.test(method) && hasKey(data, model.__key) && new RegExp(data[model.__key]).test(url) === false) {
-	// 	url += '/' + data[model.__key];
-	// 	delete data[model.__key];
-	// }
-
-	// try {
-	// 	file.mkdirSync(__dirname + '/../.tmp');
-	// } catch(e) {};
-
-
-	// try {
-	// 	file.openSync(model.$baseUrl() +  + model.__name)
-	// } catch(e) {
-	// 	console.log('open', e);
-	// }
-
-	// request[method](url)[/get/i.test(method) ? 'query' : 'send'](data)
-	// 	.set(model.$headers())
-	// 	.type('json')
-	// 	.accept('json')
-	// 	.end(function(_error, _res) {
-	// 		var res = is.an.object(_res) ? _res : {},
-	// 			body = hasKey(_res, 'body') && (is.object(_res.body) || is.array(_res.body)) ? _res.body : null;
-
-	// 		if (res['ok'] !== true) {
-	// 			error = new Error('The response from the server was not OK');
-	// 		}
-
-	// 		if (body === null) {
-	// 			error = new Error('The response from the server contained an empty body');
-	// 		}
-
-	// 		_callback && _callback(error, body);
-
-	// 	});
-
 }
